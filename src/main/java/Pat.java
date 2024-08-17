d676b19f309a5881d5d247b1b884a068e74e038d
xenthm <56202777+xenthm@users.noreply.github.com>
2024-08-17 22:53:45 +0800

    Rename to Pat
    Only done for main source file for now.

---
+++ src/main/java/Pat.java
@@ -0,0 +1,9 @@
+public class Pat {
+    public static void main(String[] args) {
+        System.out.println("""
+                Hello! This is Pat!
+                What can I do for you?
+                """);
+        System.out.println("Bye! Hope to see you again!");
+    }
+}
