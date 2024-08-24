a76a4466e2c2c091ba15dc2f33a5ea5012d4e912
xenthm <56202777+xenthm@users.noreply.github.com>
2024-08-24 19:07:22 +0800

    Decompose Pat's messages and add colour
    Move Pat's greeting and goodbye into their own methods for modularity.
    
    Add colour to Pat's messages to differentiate from the user.

--- src/main/java/Pat.java
+++ src/main/java/Pat.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
+
 public class Pat {
-    public static void main(String[] args) {
-        System.out.println("""
+    // ANSI escape code to change Pat's messages to pink
+    public static final String PINK = "\033[95m";
+    public static final String RESET = "\033[0m";
+
+    private static void say(String msg) {
+        System.out.println(PINK + msg + RESET);
+    }
+
+    private static void greet() {
+        say("""
                 Hello! This is Pat!
                 What can I do for you?
                 """);
-        System.out.println("Bye! Hope to see you again!");
+    }
+
+    private static void bye() {
+        say("Bye! Hope to see you again!");
+    }
+
+    public static void main(String[] args) {
+        greet();
+        bye();
     }
 }
