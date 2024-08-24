3bbe8441dbdaaa49743c6c19d2159337088be5c0
xenthm <56202777+xenthm@users.noreply.github.com>
2024-08-24 20:27:28 +0800

    Echo user messages
    Add ANSI escape character sequences to manipulate the output of the app
    for a cleaner user experience. This only works in the native OS terminal.

--- src/main/java/Pat.java
+++ src/main/java/Pat.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Pat {
     // ANSI escape code to change Pat's messages to pink
@@ -12,16 +13,33 @@ public class Pat {
     private static void greet() {
         say("""
                 Hello! This is Pat!
-                What can I do for you?
-                """);
+                What can I do for you?""");
     }
 
     private static void bye() {
-        say("Bye! Hope to see you again!");
+        say("Bye! Remember, this is Pat!");
+    }
+
+    private static void runChat() {
+        String line;
+        Scanner in = new Scanner(System.in);
+        boolean saidBye = false;
+        do {
+            System.out.print("Type your message: ");
+            line = in.nextLine();
+            System.out.print("\033[F\033[1G\033[2K" + line + "\033[E");
+            if (line.equals("bye")) {
+                saidBye = true;
+            } else {
+                say(line);
+            }
+        } while (!saidBye);
     }
 
     public static void main(String[] args) {
+        System.out.println("(Type \"bye\" to end the chat)");
         greet();
+        runChat();
         bye();
     }
 }
