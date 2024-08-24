3938c1b74701872683eab6bb03602cbd044198d2
xenthm <56202777+xenthm@users.noreply.github.com>
2024-08-24 20:52:38 +0800

    Add add task to todo and list tasks methods

--- src/main/java/Pat.java
+++ src/main/java/Pat.java
@@ -1,7 +1,10 @@
+import java.lang.reflect.Array;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Pat {
+    private static ArrayList<String> todoList = new ArrayList<>();
+
     // ANSI escape code to change Pat's messages to pink
     public static final String PINK = "\033[95m";
     public static final String RESET = "\033[0m";
@@ -20,6 +23,18 @@ public class Pat {
         say("Bye! Remember, this is Pat!");
     }
 
+    private static void addToTodo(String task) {
+        todoList.add(task);
+        say("Added: " + task);
+    }
+
+    private static void printList() {
+        say("Here's your todo list!");
+        for (int i = 0; i < todoList.size(); i++) {
+            say((i + 1) + ". " + todoList.get(i));
+        }
+    }
+
     private static void runChat() {
         String line;
         Scanner in = new Scanner(System.in);
@@ -27,11 +42,18 @@ public class Pat {
         do {
             System.out.print("Type your message: ");
             line = in.nextLine();
+            if (line.trim().isEmpty()) {
+                System.out.print("\033[F");
+                continue;
+            }
             System.out.print("\033[F\033[1G\033[2K" + line + "\033[E");
+
             if (line.equals("bye")) {
                 saidBye = true;
+            } else if (line.equals("list")) {
+                printList();
             } else {
-                say(line);
+                addToTodo(line);
             }
         } while (!saidBye);
     }
