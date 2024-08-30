6dd1182c6788c2ea3b378c9828282142cac91863
xenthm <56202777+xenthm@users.noreply.github.com>
2024-08-31 01:32:11 +0800

    Add inherited classes for polymorphism
    Add Todo, Event, and Deadline classes for more fine-tuned behaviour.

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,13 @@
+public class Deadline extends Task {
+    private String by;
+
+    public Deadline(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+
+    @Override
+    public String getTask() {
+        return "[D]" + super.getTask() + " (by: " + by + ")";
+    }
+}
