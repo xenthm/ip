5725797412b42f90891c634550c428a95734dc21
xenthm <56202777+xenthm@users.noreply.github.com>
2024-09-22 00:16:51 +0800

    Add JavaDoc for Task class and make constructors protected

--- src/pat/types/Task.java
+++ src/pat/types/Task.java
@@ -1,15 +1,30 @@
 package types;
 
+/**
+ * Super class for different <code>Task</code> types. Contains a description and status for whether it is marked as
+ * done or not.
+ */
 public class Task {
     protected String description;
     protected boolean isDone;
 
-    public Task(String description) {
+    /**
+     * Constructor for <code>Task</code>. Sets <code>description</code> and <code>isDone</code> to false by default.
+     *
+     * @param description Description for <code>Task</code>.
+     */
+    protected Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
-    public Task(String description, boolean isDone) {
+    /**
+     * Constructor for <code>Task</code>. Allows <code>isDone</code> to be specified.
+     *
+     * @param description Description for <code>Task</code>.
+     * @param isDone      Boolean for whether the <code>Task</code> is done or not.
+     */
+    protected Task(String description, boolean isDone) {
         this.description = description;
         this.isDone = isDone;
     }
@@ -22,6 +37,9 @@ public class Task {
         return isDone;
     }
 
+    /**
+     * Returns <code>String</code> form of <code>Task</code>.
+     */
     public String getTask() {
         return (isDone ? "[X]" : "[ ]") + " " + description;
     }
