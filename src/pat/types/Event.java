29fd059ff686441c7fdf6939d03dcb033354c053
xenthm <56202777+xenthm@users.noreply.github.com>
2024-09-22 00:33:14 +0800

    Remove unused setters in Deadline and Event classes

--- src/pat/types/Event.java
+++ src/pat/types/Event.java
@@ -24,16 +24,8 @@ public class Event extends Task {
         return from;
     }
 
-    public void setFrom(String from) {
-        this.from = from;
-    }
-
     public String getTo() {
         return to;
-    }
-
-    public void setTo(String to) {
-        this.to = to;
     }
 
     @Override
